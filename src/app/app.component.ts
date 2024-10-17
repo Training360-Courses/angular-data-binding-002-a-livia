f90af8cdb1a177f598cb6857f0c777751c276998
FPOX0F <a.livia@users.noreply.github.com>
2024-10-17 15:48:47 +0200

    gyakorlas - test-driven development complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ export class AppComponent {
   title = 'Struktúraváltás';
 
   menuItems: {title: string, link: string, order: number}[] = [
-    {title: 'Támogatás', link: '/support', order: 3},
-    {title: 'Vállalati ügyfelek', link: '/enterprise', order: 2},
     {title: 'Szolgáltatások', link: '/services', order: 1},
+    {title: 'Vállalati ügyfelek', link: '/enterprise', order: 2},
+    {title: 'Támogatás', link: '/support', order: 3},
     {title: 'Árak', link: '/prices', order: 4},
   ];
 
